9d1ed4aace4f6eef6771623929946da850f89db1
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-11 16:12:24 +0200

    complete auth support

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
-    <div>
-      <Button variant="outline">Add</Button>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
     </div>
 
   )
 }
+
+export default SetupPage;
