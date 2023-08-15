c7c4bf1e53f02f08d3cbb4409dec65af5d96a5e3
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-15 19:42:17 +0200

    add form component

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,13 +2,23 @@
 
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const formSchema = zod.object({
+    name: zod.string().min(1),
+})
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
 
+    const form = useForm<zod.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+    })
+
     return (
-        <Modal title="Create store" description="Add a new store to mangage products and categories." isOpen={storeModal.isOpen} onClose={storeModal.onClose}>
-            Create Store Form
+        <Modal title="Create store" description="Add a new store to mangage products and categories." isOpen={storeModal.isOpen} onClose={storeModal.onClose}> 
         </Modal>
     )
 };
