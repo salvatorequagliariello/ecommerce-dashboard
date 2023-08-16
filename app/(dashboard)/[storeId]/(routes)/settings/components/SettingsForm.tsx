343d4f8e8192fccd83e0f26146c0da7b1fb91581
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-16 19:47:46 +0200

    add api component

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -15,6 +15,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -103,6 +104,8 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
                     <Button disabled={loading} className="ml-auto" type="submit">Save changes</Button>
                 </form>
             </Form>
+            <Separator />
+            <ApiAlert title="NEXT_PUBLIC_API_URL" description={`${origin}/api/${params.storeId}`} variant="public"/>
         </>
     );
 };
