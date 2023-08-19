a78e967e0006c7fbdd165a6081cf335928f36122
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-19 16:38:59 +0200

    fix search bar in product client

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -32,7 +32,7 @@ export const ProductsClient: React.FC<ProductsClientProps> = ({
                 </Button>
             </div>
             <Separator />
-            <DataTable searchKey="label" columns={columns} data={data} />
+            <DataTable searchKey="name" columns={columns} data={data} />
             <Heading title="API" description="API calls for Products"  />
             <Separator />
             <ApiList entityName="products" entityIdName="productId" />
