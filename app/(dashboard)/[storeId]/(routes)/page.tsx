9592a42e5022484bd4f58832b8b19d337c9f28c7
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-09-03 19:46:42 +0200

    add complete store dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -7,6 +7,8 @@ import { formatter } from "@/lib/utils";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
 import { getSalesCount } from "@/actions/get-sales-count";
 import { getStockCount } from "@/actions/get-stock-count copy";
+import { Overview } from "@/components/Overview";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
 
 interface DashboardPageProps {
     params: { storeId: string }
@@ -18,13 +20,14 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
     const totalRevenue = await getTotalRevenue(params.storeId);
     const salesCount = await getSalesCount(params.storeId);
     const stockCount = await getStockCount(params.storeId);
+    const graphData = await getGraphRevenue(params.storeId);
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
                 <Heading title="Dashboard" description="Overview of your store" />
                 <Separator />
-                <div className="grid gap-4 grid-cols-3">
+                <div className="grid gap-4 grid-cols-1 min-[450px]:grid-cols-2 sm:grid-cols-3">
                     <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                             <CardTitle className="text-sm font-medium">
@@ -65,6 +68,14 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                         </CardContent>
                     </Card>
                 </div>
+                <Card className="col-span-4">
+                    <CardHeader>
+                        <CardTitle>Overview</CardTitle>
+                    </CardHeader>
+                    <CardContent className="pl-2">
+                        <Overview data={graphData} />
+                    </CardContent>
+                </Card>
             </div>
         </div>
     );
